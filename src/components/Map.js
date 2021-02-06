6c09d457d59d780babc3b6f03ccc88eec55fecef
jcpleitez95 <jcpleitez@outlook.com>
2021-02-06 15:20:17 -0500

    camera effect is working

--- src/components/Map.js
+++ src/components/Map.js
@@ -3,9 +3,11 @@ import React from 'react'
 export default function Map({position, map}) {
     console.log(position.y)
     return (
-        <img className="map" src={map} alt="map" style={{
-           bottom: position.x,
-           right: position.y
-        }}/>
+        <div>
+            <img className="map" src={map} alt="map" style={{
+                bottom: position.x,
+                right: position.y
+            }}/>
+        </div>
         )
     }
